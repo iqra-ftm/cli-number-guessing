ca5c4ea0850c61fdc52b151bf6db9bbfd68adee0
iqra-ftm <iqra.akhter00@gmai.com>
2024-03-29 10:45:26 +0500

    created number guessing game

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// 1) computer will generate a random number - Done.
+// 2) User input for guessing number - Done.
+// 3) compare user input with computer generate number and show result
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number between 1-10: ",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("congtratulation! you guesses right number.");
+}
+else {
+    console.log("you guessed wrong number.");
+}
